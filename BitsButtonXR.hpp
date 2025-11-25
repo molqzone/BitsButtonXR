41b8320d4176c154aa65cc0df16b4802660a020b
keruth <k3ruth@outlook>
2025-11-26 00:49:54 +0800

    chore: initial commit

---
+++ BitsButtonXR.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// clang-format off
+/* === MODULE MANIFEST V2 ===
+module_description: No description provided
+constructor_args: []
+template_args: []
+required_hardware: []
+depends: []
+=== END MANIFEST === */
+// clang-format on
+
+#include "app_framework.hpp"
+
+class BitsButtonXR : public LibXR::Application {
+public:
+  BitsButtonXR(LibXR::HardwareContainer &hw, LibXR::ApplicationManager &app) {
+    // Hardware initialization example:
+    // auto dev = hw.template Find<LibXR::GPIO>("led");
+  }
+
+  void OnMonitor() override {}
+
+private:
+};
